74995ba5b9d0f699ca734b45e23d5715c0048e00
Chenyu Li <65836534+ChenyuLi0627@users.noreply.github.com>
2024-09-13 10:04:59 +0800

    Add files via upload

---
+++ Cut&Tag/standard_pipeline/04_Cut&tag_1k_coverage.sh
@@ -0,0 +1,36 @@
+#/bin/bash
+#PBS -N cuttag coverage
+#PBS -l nodes=cu18:ppn=1,mem=40gb
+#PBS -e /home/Lichenyu/qsub_log
+#PBS -o /home/Lichenyu/qsub_log
+#PBS -q cu
+#PBS -t 2
+
+# Kill script if any commands fail
+set -e
+echo "Job starts at:"
+date
+
+OUTPUT=/home/Lichenyu/bin_coverage/tmp
+echo "job start at:"
+date
+# set working directory
+
+
+uid="rnaqc" #user id
+ls_date=`date +m%d%H%M%S` 
+cd /tmpdisk  
+Work_dir=${uid}_${ls_date}_${PBS_ARRAYID} 
+mkdir ${Work_dir} 
+cd $Work_dir 
+######################Scripts###################
+inputfile=`cat /home/Lichenyu/cuttag_bam/name.txt |head -${PBS_ARRAYID}|tail -1`
+name=`echo $inputfile|awk -F"/" '{print $NF}' |sed s/_bowtie2.mapped.bam//g` 
+~/soft/bedtools coverage -sorted  -a ~/ref/DRC.MotherHap.1k.bed  -b /home/Lichenyu/cuttag_bam/${name}_bowtie2.mapped.bam  |awk '{print $1"\t"$2"\t"$3"\t"$4}' >  ${name}.bincoverage
+
+cp * ${OUTPUT}
+
+cd /tmpdisk
+rm -rf ${Work_dir} 
+echo "The program normally finished at"
+date
