eb708d84f4e041e727f2ce0accdd60aa5a9bf629
Chenyu Li <65836534+ChenyuLi0627@users.noreply.github.com>
2024-09-13 10:41:41 +0800

    Delete Hi-C/standard pipeline/Compartment directory

--- Hi-C/standard pipeline/Compartment/01_homer_reference.sh
+++
@@ -1,4 +0,0 @@
-#Generation of homer for pig annotation files using reference genomes and annotation files
-ref= #reference genome
-gtf= #gtf of reference genome
-singularity exec miniconda_homer_R.sif loadGenome.pl -name ref -org null -fasta $ref -gtf $gtf
